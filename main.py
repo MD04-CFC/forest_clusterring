937af43cb13a6467b720f5d57aaafa3e01a68453
Maciej <maciek.dabrowski.2004@gmail.com>
2025-03-12 17:53:53 +0100

    3d cluster1

--- main.py
+++ main.py
@@ -5,14 +5,28 @@ covertype = fetch_ucirepo(id=31)
   
 # data (as pandas dataframes) 
 X = covertype.data.features 
+X2 = X.copy()
 y = covertype.data.targets 
   
-# metadata 
-print(covertype.metadata) 
-  
-# variable information 
-print(covertype.variables) 
 
-print(X[200:250])
-print(X['Slope'])
-print(max(X['Slope']))
+
+
+import pandas as pd
+from sklearn.cluster import KMeans
+import plotly.express as px
+
+km = KMeans(n_clusters = 6)
+
+    
+X2['cluster'] = km.fit_predict(X) 
+X2['cluster'] = X2['cluster'].astype('category')
+
+
+
+fig = px.scatter_3d(X2,
+                    x='Elevation',
+                    y='Aspect',
+                    z='Slope',
+                    color='cluster')
+
+fig.show(renderer='browser')
